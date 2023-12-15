34b5d034bf8540c9f6cd986c1fd2692ceb4f1cb7
OliveSeal <oliverplayz34@gmail.com>
2023-12-15 10:21:35 +0100

    1st Movie work :)

--- JS/Js.js
+++ JS/Js.js
@@ -1,8 +1,8 @@
 // Importerer intitalizApp fra Firebase-app SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
 // Importerer alle Firestore-funksjonene vi skal bruke 
-// Om du får feilmeldinga "ReferenceError: [...] is not defined", kan det være det fordi du har brukt en Firestore-funksjon uten å ha importert den her.
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js"
+// Om du får feilmeldinga "ReferenceError: [...] is not defined, kan det være det fordi du har brukt en Firestore-funksjon uten å ha importert den her.
+import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDv1ecq8S79fPsnlwl3KRzJeW70fQ_ywtA",
@@ -14,30 +14,51 @@ const firebaseConfig = {
   measurementId: "G-DH6LNR0FPN"
 };
 
+
 // Starter opp Firebase med den angitte konfigurasjonen
 const app = initializeApp(firebaseConfig);
 // Henter ut referanse til Firestore-databasen
 const db = getFirestore();
 
-import { setDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
+// Reference to the "movies" collection in Firestore
+const moviesRef = doc(db, 'Film', 'Dragon Ball Super: Broly');
 
-// Lager et nytt dokument i samlinga "elever"
-await setDoc(
-    doc(db, "Film", "Blyat"), {
-      FilmNamn: "Blyat",
-      Utgitt: "2001",
-      Direktør: "Cyka Blyat"    
-  });
- 
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
+// Function to fetch and display movies
+function displayMovies() {
+    const movieContainer = document.querySelector('.movie-container');
 
-const snapshot = await getDocs(
-  collection(db, "Film")
-);
+    onSnapshot(moviesRef, (docSnapshot) => {
+        if (docSnapshot.exists()) {
+            const movieData = docSnapshot.data();
 
-snapshot.forEach((docSnap) => {
-  console.log(
-    docSnap.data().FilmNamn, 
-    docSnap.data().Utgitt
-  );
-}); 
+            // Create movie card
+            const movieCard = document.createElement('div');
+            movieCard.className = 'movie-card';
+
+            // Create movie image
+            const movieImage = document.createElement('img');
+            movieImage.className = 'movie-image';
+            movieImage.src = movieData.Bilde;
+            movieImage.alt = movieData.FilmNamn;
+
+            // Create movie info
+            const movieInfo = document.createElement('div');
+            movieInfo.className = 'movie-info';
+            movieInfo.innerHTML = `
+                <h2>${movieData.FilmNamn}</h2>
+                <p>Year: ${movieData.Utgitt}</p>
+                <p>Director: ${movieData.Direktør}</p>
+            `;
+
+            // Append image and info to the card
+            movieCard.appendChild(movieImage);
+            movieCard.appendChild(movieInfo);
+
+            // Append the movie card to the container
+            movieContainer.appendChild(movieCard);
+        }
+    });
+}
+
+// Call the displayMovies function to fetch and display movies when the page loads
+window.onload = displayMovies;
